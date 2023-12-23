4d0811adc19a1954750f14dcad53e9c9d289db5f
PlungedInCode <plungedincode@gmail.com>
2023-12-23 22:20:03 +0300

    Logger

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <string>
 #include "UI/UIManager.hpp"
 #include "Logger.hpp"
-#include "DB/Utils.hpp"
+// #include "DB/Utils.hpp"
 
-using namespace Utils;
+// using namespace Utils;
 
 int main() {
-  // std::string DBTEST_folderPath = "music/DBTEST";
-  // std::string Fulls_folderPath = "music/Fulls";
-  std::string Samples_folderPath = "music/Samples";
-  // std::string SamplesTest_folderPath = "music/SamplesTEST";
 
-  DataBase db("TESTDB.db");
+  Logger::DisableFileStamp();
+  Logger::DisableTimeStamp();
 
-  try {
-    // LoadFolder(db, folderPath);
-    TestFolder(db, Samples_folderPath);
-  } catch (const std::exception& e) {
-    std::cerr << "Error: " << e.what() << std::endl;
-    return 1;
-  }
+  ShazamUI app;
+  app.Run();
+
+  // // std::string DBTEST_folderPath = "music/DBTEST";
+  // // std::string Fulls_folderPath = "music/Fulls";
+  // std::string Samples_folderPath = "music/Samples";
+  // // std::string SamplesTest_folderPath = "music/SamplesTEST";
+
+  // DataBase db("TESTDB.db");
+
+  // try {
+  //   // LoadFolder(db, folderPath);
+  //   TestFolder(db, Samples_folderPath);
+  // } catch (const std::exception& e) {
+  //   std::cerr << "Error: " << e.what() << std::endl;
+  //   return 1;
+  // }
 
   return 0;
 }
